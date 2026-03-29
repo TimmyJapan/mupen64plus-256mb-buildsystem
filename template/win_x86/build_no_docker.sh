da24817e84b5e41b75bc017e8e4d2f22fcc03f5d
Timmy6259 <144129012+TimmyJapan@users.noreply.github.com>
2026-03-29 15:32:10 +0900

    Fix patch path in build_no_docker.sh script

--- template/win_x86/build_no_docker.sh
+++ template/win_x86/build_no_docker.sh
@@ -10,7 +10,7 @@ echo "Building Mupen64Plus Core..."
 git clone https://github.com/mupen64plus/mupen64plus-core.git
 cd mupen64plus-core
 # PATCHESディレクトリは元のリポジトリ構成に合わせて参照
-git apply ../../PATCHES/core_256mb.patch
+git apply ../PATCHES/core_256mb.patch
 cd projects/unix
 # MinGW 32bitを指定してビルド
 make all -j$(nproc) OS=MINGW CPU=i686 CC=i686-w64-mingw32-gcc CXX=i686-w64-mingw32-g++
